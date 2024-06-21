9b9ebc67f2a8ec4686d36ad93c7fc03d8524996e
Akash Rawat <akkurwt002@gmail.com>
2024-06-21 23:51:45 +0530

    commit-fixed

--- src/components/Recentsearch.jsx
+++ src/components/Recentsearch.jsx
@@ -5,29 +5,29 @@ const Recentsearch = () => {
   const searchItems = [
     { from: "Bangalore", to: "Delhi" },
     { from: "Mumbai", to: "Chennai" },
-    { from: "Kolkata", to: "Hyderabad" },
-    { from: "Pune", to: "Ahmedabad" }
+    { from: "Kolkata", to: "Hyderabad" }
   ];
 
   return (
     <section className='pt-5'>
-                   <div className="recent-search-heading">
+      <div className="recent-search-heading">
         <h4>Recent Searches</h4>
       </div>
       <div className="recent-search-container">
-      {searchItems.map((item, index) => (
-        <div className="date-box" key={index}>
-            <div className='checked=date'>
-                <h5>21 June, Friday</h5>
+        {searchItems.slice(0, 3).map((item, index) => (
+          <div className="date-box" key={index}>
+            <div className='checked-date'>
+              <h5>21 June, Friday</h5>
             </div>
-          <div className="recent-box">
-            <h6>{item.from}</h6>
-            <ConnectingAirportsIcon className="icon" />
-            <h6>{item.to}</h6>
+            <div className="recent-box">
+              <h6>{item.from}</h6>
+              <ConnectingAirportsIcon className="icon" />
+              <h6>{item.to}</h6>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+        <div className="placeholder"></div>
+      </div>
     </section>
   );
 }
